7eb5954c147bffd8e644cdb8accf9c10b7c6caa1
Jeroen Visser <jeroenvisser101@gmail.com>
2016-06-02 17:07:21 +0200

    Change the object registry, allow SafeParams

--- lib/no_brainer/document/core.rb
+++ lib/no_brainer/document/core.rb
@@ -1,23 +1,13 @@
 module NoBrainer::Document::Core
   extend ActiveSupport::Concern
 
-  singleton_class.class_eval { attr_accessor :_all }
-  self._all = []
-
   included do
     # TODO test these includes
     extend ActiveModel::Naming
     extend ActiveModel::Translation
 
     unless name =~ /^NoBrainer::/
-      if NoBrainer::Document::Core._all.map(&:name).include?(name)
-        raise "Fatal: The model `#{name}' is already registered and partially loaded.\n" +
-              "This may happen when an exception occured while loading the model definitions\n" +
-              "(e.g. calling a missing class method on another model, having circular dependencies).\n" +
-              "In this situation, ActiveSupport autoloader may retry loading the model.\n" +
-              "Try moving all class methods declaration at the top of the model."
-      end
-      NoBrainer::Document::Core._all << self
+      NoBrainer::Loader.register(self)
     end
   end
 
@@ -26,7 +16,7 @@ module NoBrainer::Document::Core
       case type
       when :user
         Rails.application.eager_load! if defined?(Rails.application.eager_load!)
-        _all
+        NoBrainer::Loader.registry
       when :nobrainer
         [NoBrainer::Document::Index::MetaStore, NoBrainer::Lock]
       when :system
--- lib/no_brainer/loader.rb
+++ lib/no_brainer/loader.rb
@@ -1,5 +1,26 @@
 module NoBrainer::Loader
-  def self.cleanup
-    NoBrainer::Document::Core._all.clear
+  REGISTRY = Concurrent::Array.new
+
+  class << self
+    def clear_registry
+      REGISTRY.clear
+    end
+
+    def register(klass)
+      delete(klass) if include?(klass)
+      REGISTRY << klass
+    end
+
+    def registry
+      REGISTRY
+    end
+
+    def delete(klass)
+      REGISTRY.delete(klass)
+    end
+
+    def include?(klass_name)
+      REGISTRY.map(&:name).include?(klass_name)
+    end
   end
 end
